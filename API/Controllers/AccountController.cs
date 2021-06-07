167264475113e4feffe39e2152b78fa4d332ed52
Michael Lauritsen <mla@complyto.com>
2021-06-07 07:39:35 +0200

    added dto + register controller

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO)
+        {
+            if(await UserExists(registerDTO.Username)) return BadRequest("Username is taken!");
+
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = registerDTO.Username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(u => u.UserName == username.ToLower());
+        }
+    }
+}
---
+++ API/DTOs/RegisterDTO.cs
@@ -0,0 +1,8 @@
+namespace API.DTOs
+{
+    public class RegisterDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
